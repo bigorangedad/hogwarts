f6fd0eadf02a6b211d5c0896e333b232628ddf86
bigorangedad <454701211@qq.com>
2020-06-15 22:37:54 +0800

    更新list.py

--- test_selenium.py
+++ test_selenium.py
@@ -1,1 +1,16 @@
+import selenium
 from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+def test_selenium ():
+    # driver = webdriver.Chrome()
+    driver.get("http://www.baidu.com")
+
+test_selenium()
+
+#
+# if __name__ == '__main__':
+#     pytest.main()
+
+
